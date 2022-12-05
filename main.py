708888f070a7b98d47841ecf271190ce6a85d28a
ioannisbargiotas <65168157+ioannisbargiotas@users.noreply.github.com>
2022-12-05 12:27:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -181,19 +181,18 @@ if __name__ == "__main__":
     mf = np.arange(1,mf_end)
     mf = np.append(10,mf.shape[0])
     mf_iter = mf.min()
-    max_features = [int(x) for x in np.linspace(1,mf_end,num = mf_iter)]#np.arange(1,int(np.round(0.7*maxnumPTS)+1))#
+    max_features = np.arange(int(np.round(0.3*maxnumPTS),int(np.round(0.7*maxnumPTS)+1))#[int(x) for x in np.linspace(1,mf_end,num = mf_iter)]#
     # Maximum number of levels in tree
     #max_depth = 'None'#np.arange(3,8,2)
     # Minimum number of samples required to split a node
-    min_samples_split = np.linspace(2/maxMinLS, 22/maxMinLS, num = 5)
+    #min_samples_split = np.linspace(2/maxMinLS, 22/maxMinLS, num = 5)
     # Minimum number of samples required at each leaf node
     msl = np.arange(1,int(np.round(0.5*maxMinLS)+1))
     msl = np.append(10,msl.shape[0])
     msl_iter = mf.min()
-    min_samples_leaf = [int(x) for x in np.linspace(1, int(np.round(0.5*maxMinLS)+1), num = msl_iter)]#np.arange(1,int(np.round(0.2*maxMinLS)+1))#
+    min_samples_leaf = np.arange(1,int(np.round(np.sqrt(maxMinLS)+1)))#[int(x) for x in np.linspace(1, int(np.round(0.5*maxMinLS)+1), num = msl_iter)]#
     # Create the random grid
     random_grid = {'max_features': max_features,
-                   'min_samples_split':  min_samples_split,
                    'min_samples_leaf': min_samples_leaf}
     
     #Grid search with 1fold as CV schema (reminder:we use the OOB decision score for Validation) (see Silke Janitza and Roman Hornung 2018)
